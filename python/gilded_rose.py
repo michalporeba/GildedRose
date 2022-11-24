b41a55b10652ae69e2dc508014edf40199e6e320
Michał Poręba <michalporeba@gmail.com>
2022-11-24 12:31:07 +0000

    Wrapping the item in a decorator (7min)

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -11,17 +11,17 @@ MAX_QUANTITY = 50
 class GildedRose(object):
 
     def __init__(self, items):
-        self.items = items
+        self.items = [self._decorate_item(item) for item in items]
 
-    def _process_aged_brie(self, item):
-        if item.quality > MIN_QUANTITY:
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.quality = item.quality - 1
+    def _decorate_item(self, item):
+        return ItemWrap(item)
 
     def update_quality(self):
         for item in self.items:
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                self._process_aged_brie(item)
+                if item.quality > MIN_QUANTITY:
+                    if item.name != "Sulfuras, Hand of Ragnaros":
+                        item.quality = item.quality - 1
             else:
                 if item.quality < MAX_QUANTITY:
                     item.quality = item.quality + 1
@@ -57,3 +57,37 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+# Since I cannot change the Item class I will try to use the decorator pattern
+
+class ItemWrap(Item):
+    _item: Item = None 
+
+    def __init__(self, item):
+        self._item = item 
+
+    def __repr__(self):
+        return super().__repr__()
+
+    @property
+    def name(self):
+        return self._item.name
+
+    @property 
+    def sell_in(self):
+        return self._item.sell_in 
+
+    @sell_in.setter 
+    def sell_in(self, value):
+        self._item.sell_in = value 
+
+    @property 
+    def quality(self):
+        return self._item.quality 
+
+    @quality.setter 
+    def quality(self, value):
+        self._item.quality = value 
+
+
+    
